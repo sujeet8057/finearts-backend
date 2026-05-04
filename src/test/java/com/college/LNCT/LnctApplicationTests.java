3cbcfcbf11f22d2e110a7c2ccffff6a26d5c2db5
Sujeet Patel <sp8057067@gmail.com>
2026-05-04 17:39:15 +0530

    fixed single admin problem

--- src/test/java/com/college/LNCT/LnctApplicationTests.java
+++ src/test/java/com/college/LNCT/LnctApplicationTests.java
@@ -2,12 +2,21 @@ package com.college.LNCT;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class LnctApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void generatePassword() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "Admin@098";
+        String encodedPassword = encoder.encode(rawPassword);
+
+        System.out.println("Encoded Password: " + encodedPassword);
+    }
 }
